b015f51216b8e198789e54d48bc41d3ebb5b39eb
oguzhancoskun <oguzhancmd@gmail.com>
2015-02-28 09:31:41 -0500

    remove all files

--- vurt_client.py
+++ vurt_client.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python
 
-import urllib,urllib2
+import urllib,urllib2,sys,os
 
-url = 'http://hunt.tyrox.co/get.php'
-data = {'username':'oguzhan','password':'respect'}
-data = urllib.urlencode(data)
-request = urllib2.Request(url + '?' + data)
-response = urllib2.urlopen(request)
+def update():
+	url = 'http://hunt.tyrox.co/get.php'
+	data = {'username':'oguzhan','password':'respect'}
+	data = urllib.urlencode(data)
+	request = urllib2.Request(url + '?' + data)
+	response = urllib2.urlopen(request)
 
+def setup():
+	os.system('wget https://bootstrap.pypa.io/get-pip.py')
+	os.system('python get-pip.py')
+
+if(sys.argv[1]=='setup'):
+	setup()
